caed6b079ff4b32aa9173e266a135af5f8e5c701
Alaazuhd <alaazuhd17@gmail.com>
2022-12-12 14:44:14 +0200

    Generic Stack implmentation

---
+++ FTSTraining.FirstExercise/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace FTSTraining.FirstExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t \t ----------\t\twelcome To Stack Implementation Exercise!\t\t---------");
+
+            // test #1. Stack with int type. 
+            Stack<int> stack1 = new Stack<int>();
+            int[] arr1 = { 3, 4, 5 };
+            Console.WriteLine("Test NO.1: T --> int"); 
+            StaticTest(stack1, arr1);
+            Console.WriteLine("\t \t ----------\t\t----------\t\t----------\n");
+
+            // test #2. Stack with double type. 
+            Stack<double> stack2 = new Stack<double>();
+            double[] arr2 = { 3.5, 4.5, 5.5 };
+            Console.WriteLine("Test NO.2: T --> double");
+            StaticTest(stack2, arr2);
+            Console.WriteLine("\t \t ----------\t\t----------\t\t----------\n");
+
+            // test #1. Stack with string type. 
+            Stack<string> stack3 = new Stack<string>();
+            string[] arr3 = { "Alaa", "Zuhd", "FTS" };
+            Console.WriteLine("Test NO.3: T --> string");
+            StaticTest(stack3, arr3);
+            Console.WriteLine("\t \t ----------\t\t----------\t\t----------\n");
+
+
+        }
+
+        public static void StaticTest<T>(Stack<T> stack, T[] items_arr)
+        {
+            Console.WriteLine("Test for the implmentation of the stack will start.");
+
+            Console.Write("Items to be pushed into the stack are: "); 
+            foreach(T item in items_arr) { Console.Write($"{item}, "); }
+            Console.WriteLine("");
+
+            // push array items into the stack
+            foreach(T item in items_arr) { stack.Push(item); }
+            Console.WriteLine("Items were pushed into the stack");
+            // display the stack content 
+            stack.Print();
+            // remove one item from the stakc 
+            T? top_item = stack.Pop();
+            Console.WriteLine($"One Item was removed from the stack using Pop(), removed item: {top_item}");
+            // display the content of the stakc again: the last item will be noticed that it was removed. 
+            stack.Print();
+            // now get the top item from the stack. 
+            top_item = stack.Peak();
+            Console.WriteLine($"The top item in the stack is: {top_item}");
+            // clear the stack 
+            stack.Clear();
+            // trying to print the stack content but stakc is emmpty.
+            Console.WriteLine("Trying to display the stack items. However the stack is empty (It was cleared)"); 
+            stack.Print();
+            // try to pop one more item from the stack (stak is empty so as appropiate msg will be displayed)
+            Console.WriteLine("Trying to pop the last item from the stack, However the stack is empty!"); 
+            top_item = stack.Pop();
+            // the same thing will be displayed when trying to get the last item from the stack when it's empty.
+            Console.WriteLine("Trying to get the peak item from the stack. However the stack is empty!");
+            top_item = stack.Peak();
+            Console.WriteLine("Test is completed.");
+        }
+    }
+}
---
+++ FTSTraining.FirstExercise/Stack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTSTraining.FirstExercise
+{
+    // generic Stack class. 
+    public class Stack<T>
+    {
+
+
+        List<T> stack = new List<T>();
+
+        public void Push(T item)
+        {
+            stack.Add(item);
+        }
+
+        public T? Pop()
+        {
+            if(stack.Count > 0)
+            {
+                T item = stack.Last();
+                stack.RemoveAt(stack.Count - 1); 
+                return item;    
+            } 
+            else
+            {
+                Console.WriteLine("Stack is empty.");
+                return default(T); 
+            }
+        }
+
+        public T? Peak()
+        {
+            if (stack.Count > 0)
+            {
+                return stack.Last();
+            }
+            else
+            {
+                Console.WriteLine("Stack is empty.");
+                return default(T);
+            }
+        }
+
+        public void Print()
+        {
+            if (stack.Count > 0)
+            {
+                Console.WriteLine("Stack items are: ");
+                for (int i = 0; i < stack.Count; i++)
+                {
+                    Console.Write($"Stack[{i}]: {stack[i]}\t");
+                }
+                Console.WriteLine("");
+            } else
+            {
+                Console.WriteLine("Stack is Empty.");
+            }
+        }
+
+        public void Clear()
+        {
+            if (stack.Count == 0)
+                Console.WriteLine("Stack is already Empty.");
+            else
+            {
+                while (stack.Count > 0)
+                {
+                    stack.RemoveAt(stack.Count - 1);
+                }
+                Console.WriteLine("Stack was cleared... Stack is empty now.");
+            }
+        }
+
+    }
+}
